e49eb514c1fda3a61518fe93f63a18362c1f09b4
Arman <teseleevarman@gmail.com>
2025-08-06 18:11:49 +0300

    NewPortal:added models to the admin

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Post, PostCat, Comment, Category, Author
 
-# Register your models here.
+admin.site.register(Post)
+admin.site.register(PostCat)
+admin.site.register(Author)
+admin.site.register(Comment)
+admin.site.register(Category)
