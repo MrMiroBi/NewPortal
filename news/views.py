4540178708d04f2221fdefcf5cf0b730161c8bd3
Arman <teseleevarman@gmail.com>
2025-08-06 19:04:26 +0300

    NewsPortal-viws: Add Listview and Detailsview

--- news/views.py
+++ news/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-# Create your views here.
+
+class NewsList(ListView):
+    model = Post
+    template_name = 'newslist.html'
+    context_object_name = 'newslist'
+
+
+class NewsDetail(DetailView):
+    model = Post
+    template_name = 'newsdetail.html'
+    context_object_name = 'newsdetail'
--- project/urls.py
+++ project/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-   path('admin/', admin.site.urls),
-   path('pages/', include('django.contrib.flatpages.urls')),  # < вот тут
+    path('admin/', admin.site.urls),
+    path('pages/', include('django.contrib.flatpages.urls')),  # < вот тут
+    path('news/', include('news.urls'))
 ]
