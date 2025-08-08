0f543606cdcce660bafdda58be5fa13460488715
Arman <teseleevarman@gmail.com>
2025-08-08 12:35:25 +0300

    NewPortal-page_detail:Improve news views, template rendering, and add custom filters

--- news/views.py
+++ news/views.py
@@ -3,15 +3,21 @@ from django.views.generic import ListView, DetailView
 from .models import Post
 
 
-
 class NewsList(ListView):
     model = Post
+    ordering = '-date'
     template_name = 'newslist.html'
     context_object_name = 'newslist'
+
+    def get_queryset(self):
+        return Post.objects.filter(post_type = 'NS')
 
 
 class NewsDetail(DetailView):
     model = Post
     template_name = 'newsdetail.html'
     context_object_name = 'newsdetail'
+
+    def get_queryset(self):
+        return Post.objects.filter(post_type = 'NS')
 
