00efe1766d2d47f1b68eedcd77b85a8a5f6d3a2d
Arman <teseleevarman@gmail.com>
2025-08-09 16:57:22 +0300

    Newportal-custom_filter: add custom censor filter for content moderation

--- news/templatetags/custom_filters.py
+++ news/templatetags/custom_filters.py
@@ -1,6 +1,18 @@
 from django import template
+import re
 
 register = template.Library()
 
+@register.filter()
+def censor(value):
+    unwanted_words = ["фреймворк", "django"]
 
+    def replace_word(match):
+        word = match.group()
+        if len(word) > 1:
+            return word[0] + '*' * (len(word) - 1)
+        return word
 
+    pattern = r'\b(' + '|'.join(re.escape(word) + r'\w*' for word in unwanted_words) + r')\b'
+
+    return re.sub(pattern, replace_word, value, flags=re.IGNORECASE)
